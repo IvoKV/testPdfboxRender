d77dd898e663cb3cfa5142e6c3d22237f06f8a48
IvoKV <ivo.kristensjo-vukmanovic@sll.se>
2025-04-04 12:19:03 +0200

    Following video example

--- src/main/java/org/ivo/testpdfboxrender/DataImport.java
+++ src/main/java/org/ivo/testpdfboxrender/DataImport.java
@@ -4,7 +4,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import static org.ivo.testpdfboxrender.TestPdfboxRenderApplication.pdfFileName;
 
 public class DataImport {
 
@@ -17,23 +16,24 @@ public class DataImport {
     public String importTestPDF() throws IOException {
         //File file = new File(pdfFileName);
         //Files.readString(Path.of())
-        InputStream is = new FileInputStream(pdfFileName);
-        StringBuilder sb = new StringBuilder(10000);
+//        InputStream is = new FileInputStream(pdfFileName);
+//        StringBuilder sb = new StringBuilder(10000);
 
-        int count = 30;
-        while(is.available() > 0) {
-            int read = is.read();
-            sb.append((char)read);
-            count--;
-//            if(count == 0) {
-//                is.close();
-//                break;
-//            }
-        }
-        is.close();
+//        int count = 30;
+//        while(is.available() > 0) {
+//            int read = is.read();
+//            sb.append((char)read);
+//            count--;
+////            if(count == 0) {
+////                is.close();
+////                break;
+////            }
+//        }
+//        is.close();
         //for(int i = sb.length(); i > 0; i--){
-            System.out.println(sb.toString());
+         //   System.out.println(sb.toString());
         //}
-        return sb.toString();
+       // return sb.toString();
+        return "";
     }
 }
